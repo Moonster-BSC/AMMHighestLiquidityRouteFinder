541cc5e38e8e80ae3c55c1cbdbf2adbe7a14a784
Moonster <Moonster.bsc@gmail.com>
2021-06-05 12:40:10 -0400

    work on search

--- src/driver/driver.ts
+++ src/driver/driver.ts
@@ -1,6 +1,27 @@
 import { quickClient } from "../apollo/client"
 import { getPairGraphWithLiquidityWeights } from "../util/getAllPairs"
 
+
+// Goals:
+// Shortest Path from A to B
+// With Highest liquidity
+
+let currentRoute = []
+let bestRoute = []
+let solved = false
+
 export const getShortestPathWithHighestLiquidity = async (startToken: string, endToken: string): Promise<string[]> => {
     const graph = await getPairGraphWithLiquidityWeights(quickClient)
+
+    // DFS
+    
+    // init routes
+    currentRoute.push(startToken)
+    bestRoute.push(startToken)
+
+
+
+
 }
+
+const search
--- src/util/getAllPairs.ts
+++ src/util/getAllPairs.ts
@@ -1,31 +1,27 @@
 import { getAllPairsQuery } from "../apollo/queries";
-import { quickClient } from "../apollo/client";
 import { NormalizedCacheObject } from "apollo-cache-inmemory";
 import ApolloClient from "apollo-client";
 
-// Goals:
-// Shortest Path from A to B
-// With Highest liquidity
-
-interface AdjacencyMap {
+interface AdjacencyMap<T> {
     [symbol: string]: {
-        [symbol: string]: number   
+        [symbol: string]: T   
     }
 }
 
-export const getPairGraphWithLiquidityWeights = async (client: ApolloClient<NormalizedCacheObject>): Promise<AdjacencyMap> => {
+export const getPairGraphWithLiquidityWeights = async (client: ApolloClient<NormalizedCacheObject>): Promise<AdjacencyMap<number>> => {
     const USDThreshold = 500000; // We don't want to use anything with less than 100000 liquidity or else it may incur lots of slippage
     const { data } = await client.query({
         query: getAllPairsQuery(USDThreshold)
       });
     const { pairs } = data
 
-    const pairAdjacencyMap: AdjacencyMap = {}
+    const pairAdjacencyMap: AdjacencyMap<number> = {}
 
     pairs.forEach((pair: { id: any; token0: any; token1: any; reserveUSD: any }) => {
         const { id, token0: {symbol: tokenA}, token1: {symbol: tokenB}, reserveUSD } = pair;
 
         // put in map
+        putInMap(pairAdjacencyMap, tokenA, tokenB, reserveUSD)
         putInMap(pairAdjacencyMap, tokenA, tokenB, reserveUSD)
 
         // other direction
@@ -35,11 +31,11 @@ export const getPairGraphWithLiquidityWeights = async (client: ApolloClient<Norm
     return pairAdjacencyMap
 }
 
-const putInMap = (pairAdjacencyMap: AdjacencyMap, tokenA: string, tokenB: string, reserveUSD: number) => {
+const putInMap = <T>(pairAdjacencyMap: AdjacencyMap<T>, tokenA: string, tokenB: string, data: T) => {
     if (tokenA in pairAdjacencyMap) {
-        pairAdjacencyMap[tokenA][tokenB] = reserveUSD
+        pairAdjacencyMap[tokenA][tokenB] = data
     } else {
         pairAdjacencyMap[tokenA] = {}
-        pairAdjacencyMap[tokenA][tokenB] = reserveUSD
+        pairAdjacencyMap[tokenA][tokenB] = data
     }
 }
